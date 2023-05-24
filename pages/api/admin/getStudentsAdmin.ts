a0baafd8dc4f0f0a906c8c0ad23535b3082b2a55
CrazyLeaf69 <76205967+CrazyLeaf69@users.noreply.github.com>
2023-05-24 17:30:37 +0200

    small mistake

--- pages/api/admin/getStudentsAdmin.ts
+++ pages/api/admin/getStudentsAdmin.ts
@@ -8,7 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       query: `SELECT users.*, (group_concat(DISTINCT courses.CourseId, ":", courses.Name, "grade", CASE WHEN grades.Grade IS NULL THEN 0 ELSE grades.Grade END)) Attends, CASE WHEN admins.UserId IS NULL THEN 0 ELSE 1 END IsAdmin, CASE WHEN instructors.UserId IS NULL THEN 0 ELSE 1 END IsInstructor
         FROM users
         LEFT JOIN admins ON users.UserId = admins.UserId
-        LEFT JOIN attended_courses ON attendedcourses.UserId=users.UserId
+        LEFT JOIN attended_courses ON attended_courses.UserId=users.UserId
         LEFT JOIN courses ON attended_courses.CourseId = courses.CourseId
         LEFT JOIN grades ON grades.CourseId = courses.CourseId AND grades.UserId = users.UserId
         LEFT JOIN instructors ON instructors.UserId=users.UserId
