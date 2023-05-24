4084ee34c25b9c415b3a5f3665931cd44d6f4844
CrazyLeaf69 <76205967+CrazyLeaf69@users.noreply.github.com>
2023-05-24 17:27:22 +0200

    getStudentsAdmin table names lowercase

--- pages/api/admin/getStudentsAdmin.ts
+++ pages/api/admin/getStudentsAdmin.ts
@@ -5,13 +5,13 @@ import excuteQuery from "../../../lib/db";
 export default async function handler(req: NextApiRequest, res: NextApiResponse<[StudentsAdmin]>) {
   try {
     const queryDb = await excuteQuery({
-      query: `SELECT users.*, (group_concat(DISTINCT Courses.CourseId, ":", Courses.Name, "grade", CASE WHEN Grades.Grade IS NULL THEN 0 ELSE Grades.Grade END)) Attends, CASE WHEN Admins.UserId IS NULL THEN 0 ELSE 1 END IsAdmin, CASE WHEN Instructors.UserId IS NULL THEN 0 ELSE 1 END IsInstructor
+      query: `SELECT users.*, (group_concat(DISTINCT courses.CourseId, ":", courses.Name, "grade", CASE WHEN grades.Grade IS NULL THEN 0 ELSE grades.Grade END)) Attends, CASE WHEN admins.UserId IS NULL THEN 0 ELSE 1 END IsAdmin, CASE WHEN instructors.UserId IS NULL THEN 0 ELSE 1 END IsInstructor
         FROM users
-        LEFT JOIN Admins ON users.UserId = Admins.UserId
-        LEFT JOIN AttendedCourses ON AttendedCourses.UserId=users.UserId
-        LEFT JOIN Courses ON AttendedCourses.CourseId = Courses.CourseId
-        LEFT JOIN Grades ON Grades.CourseId = Courses.CourseId AND Grades.UserId = users.UserId
-        LEFT JOIN Instructors ON Instructors.UserId=users.UserId
+        LEFT JOIN admins ON users.UserId = admins.UserId
+        LEFT JOIN attended_courses ON attendedcourses.UserId=users.UserId
+        LEFT JOIN courses ON attended_courses.CourseId = courses.CourseId
+        LEFT JOIN grades ON grades.CourseId = courses.CourseId AND grades.UserId = users.UserId
+        LEFT JOIN instructors ON instructors.UserId=users.UserId
         GROUP BY users.UserId;`,
       values: "",
     });
