9fe2e5af09cc5dcd9723d8334202babd15e0a5f2
Lowe Löwing <lowe.lowing@gmail.com>
2024-03-11 23:11:02 +0100

    Fix type annotation in getInstructors API

--- pages/api/admin/getInstructors.ts
+++ pages/api/admin/getInstructors.ts
@@ -2,14 +2,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import excuteQuery from "../../../lib/db";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<[StudentsAdmin]>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<StudentsAdmin[]>) {
   try {
-    const queryDb = await excuteQuery({
+    const queryDb = (await excuteQuery({
       query: `SELECT users.*
         FROM instructors
         INNER JOIN users ON users.UserId = instructors.UserId`,
       values: "",
-    });
+    })) as StudentsAdmin[];
+
     if (queryDb.length > 0) {
       res.status(200).json(queryDb);
     } else {
